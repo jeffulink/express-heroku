215704900489a8e3b6dbba1ce7302788750caba1
jeff <jeff@ulink.com.tw>
2017-11-02 21:43:58 +0800

    search add

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const search = require('./search')
+
 const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 const port = process.env.PORT || 3000
 
-app.get('/hi', function (req, res) {
-  res.send('Hello World!!')
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/api/search', async function (req, res) {
+  const address = req.query.address
+  if (address) {
+    try {
+      res.json(await search(address))
+    } catch (error) {
+      res.json({ error: error })
+    }
+  } else {
+    res.json({ error: 'Address is empty.' })
+  }
 })
 
 app.listen(port, function () {
--- search.js
+++ search.js
@@ -1,14 +1,16 @@
 const rp = require("request-promise")
 const getInfo = require("./getInfo")
 
+const KEY = 'AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE'
+
 const searchRestaurant = async address => {
   address = encodeURI(address)
-  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
+  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${KEY}`
 
   let addressBody = await rp(addressUrl)
   let { lat, lng } = getInfo(JSON.parse(addressBody))
   let location = `${lat},${lng}`
-  let placeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE`
+  let placeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=${KEY}`
 
   let placeBody = await rp(placeUrl)
   placeBody = JSON.parse(placeBody)
@@ -21,7 +23,4 @@ const searchRestaurant = async address => {
   return places
 }
 
-let result = searchRestaurant("台灣大學")
-result.then((places) => {
-  console.log(places)
-})
+module.exports = searchRestaurant
