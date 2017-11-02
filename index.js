e08945a02da665be289f0770c1f071927df4b2ed
jeff <jeff@ulink.com.tw>
2017-11-02 21:02:20 +0800

    hi
    hi

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
+app.get('/hi', function (req, res) {
   res.send('Hello World!!')
 })
 
